25ff9707de57f42ecac1fbea5561ab3bb55f7788
abdulfetah-kelil <abdulfetahkelil@gmail.com>
2023-12-12 23:18:27 -1200

    create router

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express=require('express');
 
 // creating express application 
 const app= express();
+app.get('/api/order', (req, res) => {
+    res.status(200).json({message: 'order'});
+})
+
+app.get('/api/profession', (req, res) => {
+    res.status(200).json({message: 'profession'});
+})
 
 // port number 
 const port=4000;
