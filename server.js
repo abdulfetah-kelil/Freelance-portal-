9bb520aaaa36075604920a958caf5374b8b82324
abdulfetah-kelil <abdulfetahkelil@gmail.com>
2023-12-13 07:38:01 -1200

    export -routers

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express=require('express');
+const routes=require('./router/routers.js');
 
-// creating express application 
+
+// creating express application by
 const app= express();
-app.get('/api/order', (req, res) => {
-    res.status(200).json({message: 'order'});
-})
 
-app.get('/api/profession', (req, res) => {
-    res.status(200).json({message: 'profession'});
-})
-
+app.use(routes);
 // port number 
 const port=4000;
 
