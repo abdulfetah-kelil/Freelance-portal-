dd81779ea84d5fc4e311119f911e07a6cf900f15
abdulfetah-kelil <abdulfetahkelil@gmail.com>
2023-12-20 22:26:34 -1200

    crate logic

--- controller/orderCon.js
+++ controller/orderCon.js
@@ -3,7 +3,12 @@ const orderFirst = (req, res) => {
 }
 
 const orderCreate = (req, res) => {
+    const {fullName, phoneNumber, email, profession, age} = req.body;
+    if (!fullName || !phoneNumber || !email || !profession || !age) {
+        throw new Error("All data must be provided!");
+    }
     res.status(200).json({message: 'order created'});
+    console.log("the created new order is :",req.body);
 }
 
 const orderUpdate = (req, res) => {
