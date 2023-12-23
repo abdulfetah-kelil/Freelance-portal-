f8363a59d862c143e8e1cca41e40c920bfe98c33
abdulfetah <abdulfetahkelil@gmail.com>
2023-12-23 16:15:19 +0400

    Add stackTrace

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -7,19 +7,19 @@ switch (statusCode) {
     res.json({title:'bad request from client'});
     break;
     case statusConst.UNAUTHORIZED:
-    res.json({title:'unauthorized request from client'});
+    res.json({title:'unauthorized request from client',stackTrace: err.stack});
     break;
     case statusConst.PAYMENT_REQUIRE:
-    res.json({title:'payment require from client error'});
+    res.json({title:'payment require from client error',stackTrace: err.stack});
     break;
     case statusConst.FOR_BIDDEN:
-    res.json({title:'for bidden from client error'});
+    res.json({title:'for bidden from client error',stackTrace: err.stack});
     break;
     case statusConst.NOT_FOUND:
-    res.json({title:'not found from client error'});
+    res.json({title:'not found from client error',stackTrace: err.stack});
     break;
     case statusConst.SERVER_ERROR:
-    res.json({title:'server error from client error'});
+    res.json({title:'server error from client error',stackTrace: err.stack});
     break;
     
 }
